e035fa0ef2349529a7db6715df89a32bd2b66457
booldook <booldook@gmail.com>
2019-05-10 11:13:14 +0900

    메인네비 ui 완료

--- js/main.js
+++ js/main.js
@@ -4,8 +4,11 @@ $(".navis > li").mouseleave(mnLeave);
 function mnOver(){
 	var src = $(this).children("img").data("hover");
 	$(this).children("img").attr("src", src);
+	$(this).find(".navi_sub").stop().slideDown(200);
 }
 function mnLeave() {
 	var src = $(this).children("img").data("src");
 	$(this).children("img").attr("src", src);
+	$(this).find(".navi_sub").stop().slideUp(200);
 }
+
